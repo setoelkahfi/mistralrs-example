d125615b12fb267f1a620d5f0749e7bc16def69d
Seto <setoelkahfi@gmail.com>
2026-02-01 08:21:42 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use anyhow::Result;
+use mistralrs::{
+    DiffusionGenerationParams, DiffusionLoaderType, DiffusionModelBuilder,
+    ImageGenerationResponseFormat,
+};
+use std::time::Instant;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let model = DiffusionModelBuilder::new(
+        "black-forest-labs/FLUX.1-schnell",
+        DiffusionLoaderType::FluxOffloaded,
+    )
+    .with_logging()
+    .build()
+    .await?;
+
+    let start = Instant::now();
+
+    let response = model
+        .generate_image(
+            "Horse, in the style of Raden Saleh, Cedric Peyravernay, Peter Mohrbacher, george clausen, artgerm, mixed media on toned paper, 2021, very detailed, coffee art.".to_string(),
+            ImageGenerationResponseFormat::Url,
+            DiffusionGenerationParams::default(),
+        )
+        .await?;
+
+    let finished = Instant::now();
+
+    println!(
+        "Done! Took {} s. Image saved at: {}",
+        finished.duration_since(start).as_secs_f32(),
+        response.data[0].url.as_ref().unwrap()
+    );
+
+    Ok(())
+}
